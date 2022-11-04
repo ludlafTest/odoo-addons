1e13a78e446148a67fdebba9f321195f697d2da4
Luis D. Lafaurie <luis.lafaurie@tecnativa.com>
2022-11-04 16:16:35 +0100

    Update __manifest__.py

--- module 1-18/__manifest__.py
+++ module 1-18/__manifest__.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
 {
+    'name': "Modo de pago en Sale Order",
 
+    'summary': """
+        Modulo de prueba para el curso de Odoo 14
+        """,
+    'description': """
+        Modulo de curso para el prueba de Odoo 14
+    """,
+
+    'author': "Lvis",
+    'website': "http://www.lvis.com",
+
+    'category': 'Tools',
+    'version': '1.0.0',
+
+    'depends': ['sale'],
+
+    # always loaded
+    'data': [
+        "views/sale_order_view.xml"
+    ],
 }
