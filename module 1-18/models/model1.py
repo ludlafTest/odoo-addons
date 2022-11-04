c922c66e0dc032db8fb266a51b64ae9807f3bd82
Luis D. Lafaurie <luis.lafaurie@tecnativa.com>
2022-11-04 16:17:50 +0100

    Update model1.py

--- module 1-18/models/model1.py
+++ module 1-18/models/model1.py
@@ -1,5 +1,17 @@
-import
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
 
 
-class SaleOrder(models.Model)
-    _inherit = "sale.order"
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    payment_mode = fields.Selection(
+        string="Payment pech mode",
+        selection=[
+            ('cash', 'Cashg'),
+            ('bank', 'Bankz'),
+            ('electronic', 'Electronich'),
+        ],
+        default="electronic",
+    )
