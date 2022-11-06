d4e721c6c19e09b78c4af7f1143569ae301f792b
Luis D. Lafaurie <luis.lafaurie@tecnativa.com>
2022-11-06 14:47:17 +0100

    Add files via upload

---
+++ sale_order_payment_mode/models/sale_order.py
@@ -0,0 +1,14 @@
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    payment_mode = fields.Selection(
+        string="Payment Mode",
+        selection=[
+            ("cash", "Cashm"),
+            ("bank", "Bankn"),
+            ("electronic", "Electronico"),
+        ],
+    )
