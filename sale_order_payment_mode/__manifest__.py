6fdd33e8630083d833d408d6a37e80c179d48da5
Luis D. Lafaurie <luis.lafaurie@tecnativa.com>
2022-11-06 14:44:26 +0100

    Add files via upload

---
+++ sale_order_payment_mode/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Modo de pago en Sale Order",
+    "summary": """
+        Modulo de prueba xa el curso de v14
+    """,
+    "description": """
+        Modulo de curso xa el prueba de v14
+    """,
+    "author": "Lvis, Tecnativa",
+    "website": "http://www.lvis.com",
+    "category": "Tools",
+    "version": "14.0.1.0.0",
+    "depends": ["sale"],
+    # always loaded
+    "data": ["views/sale_order_view.xml"],
+    "license": "AGPL-3",
+}
